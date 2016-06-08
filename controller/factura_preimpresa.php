38c020a3a0296381d0fdf45dd05db623d022a21d
Joe Nilson <joenilson@gmail.com>
2016-06-08 12:19:34 -0400

    factura_preimpresa commit inicial

---
+++ controller/factura_preimpresa.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of factura_preimpresa
+ *
+ * @author Joe Nilson <joenilson at gmail.com>
+ */
+class factura_preimpresa {
+    //put your code here
+}
