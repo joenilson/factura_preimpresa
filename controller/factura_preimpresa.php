255d1e8b9598be35a044af014e46ef1fa547fd13
Joe Nilson <joenilson@gmail.com>
2016-06-18 12:20:47 -0400

    factura_preimpresa agregadas librerias tcfpd y fpdi para lectura de pdfs

--- controller/factura_preimpresa.php
+++ controller/factura_preimpresa.php
@@ -1,5 +1,4 @@
 <?php
-
 /*
  * Copyright (C) 2016 Joe Nilson <joenilson at gmail.com>
  *
@@ -16,20 +15,45 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
+require_once 'plugins/factura_preimpresa/vendors/tcpdf/tcpdf.php';
+require_once 'plugins/factura_preimpresa/vendors/fpdi/fpdi.php';
 /**
  * Description of factura_preimpresa
+ * Proceso:
+ * Se sube un pdf para confirmar las dimensiones del mismo
+ * Se arrastran bloques de información con los valores de los campos
+ * de las tablas facturascli y lineas_facturas_cli
+ * Se guarda estas coordenadas en una tabla de formatos_preimpresos
+ * y se agregan al model de formatos_preimpresos como opciones de impresion
+ * 
+ * @todo Posibilidad de imprimir en impresoras matriciales
  *
  * @author Joe Nilson <joenilson at gmail.com>
  */
 class factura_preimpresa extends fs_controller {
-    
+    public $archivo;
+    public $pdf_import;
+    public $resultados_w;
+    public $resultados_h;
     public function __construct() {
         parent::__construct(__CLASS__, 'Diseñar Factura Preimpresa', 'admin', TRUE, TRUE, FALSE);
     }
     
     protected function private_core() {
-        
+        if($_FILES['archivo'] && $_FILES['archivo']['name']!=''){
+            $this->archivo = $_FILES['archivo']['tmp_name'];
+            $this->detectar_informacion();
+        }
+    }
+    
+    protected function detectar_informacion(){
+        $this->pdf_import = new FPDI();
+        $this->pdf_import->setSourceFile($this->archivo);
+        $tplIdx = $this->pdf_import->ImportPage(1);
+        $this->resultados_w = $this->pdf_import->getTemplateSize($tplIdx)['w'];
+        $this->resultados_h = $this->pdf_import->getTemplateSize($tplIdx)['h'];
+        //$this->pdf_import = new TCPDF_IMPORT();
+        //$this->resultados = $this->pdf_import->importPDF($this->archivo);
     }
     
     public function share_extensions(){
